3c23a73162c1a5dd88c06158e4f2750670a05a5e
Will Howell <will.s.howell@gmail.com>
2016-09-05 22:36:01 -0400

    Make that admin a little more personal

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+admin.site.site_header = 'Will Howell Portfolio Admin'
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('portfolioapp.urls')),
